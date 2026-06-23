301150fd2d8b09980066a94ea1a316751659640f
brad <brad.wils001@gmail.com>
2026-06-23 11:40:51 +1000

    first

---
+++ AVPGroupApp/AVPGroupApp/AVPGroupAppApp.swift
@@ -0,0 +1,33 @@
+//
+//  AVPGroupAppApp.swift
+//  AVPGroupApp
+//
+//  Created by brad wils on 23/6/26.
+//
+
+import SwiftUI
+
+@main
+struct AVPGroupAppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ AVPGroupApp/AVPGroupApp/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  AVPGroupApp
+//
+//  Created by brad wils on 23/6/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
